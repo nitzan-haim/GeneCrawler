ff3e53417139b664d435d5aa5fc504e1c9c10b0d
Nitzan Haim <61940253+nitzan-haim@users.noreply.github.com>
2020-04-18 23:08:11 +0300

    Update execute.py
    Exchanged crawling KnownGeneScraper with crawling HumanScraper and MouseScraper since the class KnownGeneScraper became abstract.

--- execute.py
+++ execute.py
@@ -1,11 +1,12 @@
-from known_gene_ace2_scraper import KnownGeneScraper
 from ncbi_ref_ace2_scraper import NCBIReferenceScraper
 from ref_gene_scraper import RefGeneScraper
-
+from human_scraper import HumanScraper
+from mouse_scraper import MouseScraper
 from scrapy.crawler import CrawlerProcess
 
 process = CrawlerProcess()
-process.crawl(KnownGeneScraper)
+process.crawl(MouseScraper)
+process.crawl(HumanScraper)
 process.crawl(NCBIReferenceScraper)
 process.crawl(RefGeneScraper)
 process.start()
